0f5ecf320e01159235508f184b2e41cb7b351d93
Kunal Natesh <kunal.natesh2004@gmail.com>
2026-09-15 22:13:50 +0530

    s1e23 - Added relationship between publisher and books, defined by publisher in books domain (class) and also updated Bootstrap to contain the new relationship.

--- src/main/java/com/example/demo/domain/Publisher.java
+++ src/main/java/com/example/demo/domain/Publisher.java
@@ -1,9 +1,8 @@
 package com.example.demo.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.Set;
 
 @Entity
 public class Publisher {
@@ -16,6 +15,17 @@ public class Publisher {
     private String state;
     private int zip;
 
+
+    @OneToMany(mappedBy = "publisher")
+    private Set<Book> books;
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
 
     public int getId() {
         return id;
