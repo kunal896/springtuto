579345a379dde067879a93ec75fb5389a1fffd6a
Kunal Natesh <kunal.natesh2004@gmail.com>
2026-09-18 01:34:20 +0530

    Same episode, had a few bugs, forgot @Service and a constructor for spring to inject the bookrepository. path to diagram: notes-images/MVC FLOW DIAGRAM.png

--- src/main/java/com/example/demo/service/BookServiceImpl.java
+++ src/main/java/com/example/demo/service/BookServiceImpl.java
@@ -2,11 +2,18 @@ package com.example.demo.service;
 
 import com.example.demo.domain.Book;
 import com.example.demo.repository.BookRepository;
+import org.springframework.stereotype.Service;
 
+
+@Service
 public class BookServiceImpl implements BookService{
 
     private BookRepository bookRepository;
 
+    public BookServiceImpl(BookRepository bookRepository)
+    {
+        this.bookRepository = bookRepository;
+    }
 
     @Override
     public Iterable<Book> findAll()
