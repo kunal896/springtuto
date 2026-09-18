9801bd2f6c9f36e712b4b8c1415d701ccaebad73
Kunal Natesh <kunal.natesh2004@gmail.com>
2026-09-19 00:54:17 +0530

    Working on my own, added a Get method for author creation form, and a post method that is called by the submit button in the html file author-form, uses form tag with action leading to that post method and an method attribute saying it's 'post'

--- src/main/java/com/example/demo/controllers/AuthorController.java
+++ src/main/java/com/example/demo/controllers/AuthorController.java
@@ -1,8 +1,11 @@
 package com.example.demo.controllers;
 
+import com.example.demo.domain.Author;
 import com.example.demo.service.AuthorService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,9 +18,19 @@ public class AuthorController {
     }
 
 
-    @RequestMapping("/authors")
+    @GetMapping("/authors")
     public String getAuthors(Model model){
         model.addAttribute("authors", authorService.findAll());
         return "authors";
+    }
+
+    @GetMapping("/authors/new")
+    public String displayCreateForm(){
+        return "author-form";
+    }
+
+    @PostMapping("/authors")
+    public String createAuthor(Author author){
+        return null;
     }
 }
